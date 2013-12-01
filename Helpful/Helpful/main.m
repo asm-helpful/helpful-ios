891a22507234738d8095dbb7201dbc3d7f2f9986
Ashish Awaghad <difficult.ashish@gmail.com>
2013-12-01 01:54:08 -0800

    First Commit:
    * Sign in page with username and password text fields and a sign in button
    * Username text field is enabled with keyboard on launch
    * Username text field’s keyboard will show ‘next’ option to go to the password text field
    * Password text field’s keyboard will show ‘Go’ option which will try to sign the user in.

---
+++ Helpful/Helpful/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Helpful
+//
+//  Created by Ashish Awaghad on 1/12/13.
+//  Copyright (c) 2013 Helpful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HFAppDelegate class]));
+    }
+}
